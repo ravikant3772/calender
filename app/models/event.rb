88077ea3634afdf2bafcb5fa83bb14c062e8cc46
Ravi <ravi@treeni.com>
2022-05-23 01:17:08 +0530

    add validation for valid start time and end time range

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,7 +10,18 @@ class Event
 
   validates :title, presence: true
   validates :start_time, presence: true
+	validates :start_time, presence: true
+	validate :valid_time_period
 
   belongs_to :created_by, class_name: "User"
-  has_many :user_events
+	has_many :user_events
+
+	#validate start time should be earlier than end time
+	def valid_time_period
+		if (self.start_time < self.end_time)
+			errors.add(:start_time, 'should be earlier than end time')
+			return false
+		end
+		return true
+	end
 end
